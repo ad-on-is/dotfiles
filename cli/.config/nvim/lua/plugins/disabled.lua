05d13c878f060c9d9a0e29b25430c01c92e055c2
Adis Durakovic <git@mail.adisdurakovic.com>
2025-09-24 08:45:36 +0200

    D cli/.config M cli/.config ?? cli/.config

--- cli/.config/nvim/lua/plugins/disabled.lua
+++ cli/.config/nvim/lua/plugins/disabled.lua
@@ -8,4 +8,67 @@ return {
 
   { "roobert/tailwindcss-colorizer-cmp.nvim", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Request textDocument/inlayHint failed",
+          },
+          opts = { skip = true },
+        },
+        {
+          view = "vsplit",
+          filter = { event = "msg_show", min_height = 20 },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "^/",
+          },
+          opts = { skip = true },
+        },
+
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "Nothing is copied",
+          },
+          opts = { skip = true },
+        },
+
+        {
+          filter = {
+            event = "msg_show",
+            kind = "emsg",
+            find = "E486",
+          },
+          opts = { skip = true },
+        },
+
+        {
+          filter = {
+            event = "msg_show",
+            kind = "emsg",
+            find = "E16",
+          },
+          opts = { skip = true },
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
 }
--- cli/.config/nvim/lua/plugins/ui.lua
+++ cli/.config/nvim/lua/plugins/ui.lua
@@ -357,17 +357,10 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      -- notify = {
-      --   enabled = false,
-      -- },
       lsp = {
         hover = {
           silent = true,
         },
-
-        -- signature = {
-        --   enabled = false,
-        -- },
       },
 
       cmdline = {
@@ -376,54 +369,6 @@ return {
         },
       },
 
-      routes = {
-        {
-          filter = {
-            event = "notify",
-            find = "Request textDocument/inlayHint failed",
-          },
-          opts = { skip = true },
-        },
-        {
-          view = "vsplit",
-          filter = { event = "msg_show", min_height = 20 },
-        },
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "^/",
-          },
-          opts = { skip = true },
-        },
-
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "Nothing is copied",
-          },
-          opts = { skip = true },
-        },
-
-        {
-          filter = {
-            event = "msg_show",
-            kind = "emsg",
-            find = "E486",
-          },
-          opts = { skip = true },
-        },
-
-        {
-          filter = {
-            event = "msg_show",
-            kind = "emsg",
-            find = "E16",
-          },
-          opts = { skip = true },
-        },
-      },
       -- you can enable a preset for easier configuration
       presets = {
         bottom_search = false, -- use a classic bottom cmdline for search
