1bff0c07aea61deb44131a6f956583da4bfbac74
Adis Durakovic <github@mail.adisdurakovic.com>
2026-03-05 07:10:58 +0100

    M cli/.local M gui/.config M wm/.local ?? wm/.local

--- wm/.local/scripts/wm/screenrecord.sh
+++ wm/.local/scripts/wm/screenrecord.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-SDIR="$HOME"/Videos/ScreenCaptures
+function main() {
 
-if [ ! -d "$SDIR" ]; then
-  mkdir -p "$SDIR"
-fi
-recordfile=$(ps aux | grep wl-screenrec | grep -v grep | awk '{print $(NF-0)}' | awk -F'/' '{print $(NF-0)}')
+  SDIR="$HOME"/Videos/ScreenCaptures
 
-if [ -n "$recordfile" ]; then
-  notify-send -i /tmp/screenshot.png "󰹑 Recording stopped!" " $recordfile"
-  pkill -f wl-screenrec
-  exit 0
-fi
+  if [ ! -d "$SDIR" ]; then
+    mkdir -p "$SDIR"
+  fi
+  recordfile=$(ps aux | grep wl-screenrec | grep -v grep | awk '{print $(NF-0)}' | awk -F'/' '{print $(NF-0)}')
+  screenshotfile=/tmp/screenrecord_screenshot.png
+  if [ -n "$recordfile" ]; then
+    notify-send -i $screenshotfile "󰹑 Recording stopped!" " $recordfile"
+    pkill -f wl-screenrec
+    exit 0
+  fi
 
-WORKSPACES="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
-WINDOWS="$(hyprctl clients -j | jq -r --argjson workspaces "$WORKSPACES" 'map(select([.workspace.id] | inside($workspaces)))')"
-GEOM=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | slurp -o -b 11111bdd -c e64553ff -B 11111bdd -d -w 1)
-recordfile=ScreenCapture-$(date +'%Y-%m-%d_%H:%M:%S.mp4')
-file="$SDIR/$recordfile"
-screenshotfile=/tmp/screenshot.png
+  WORKSPACES="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
+  WINDOWS="$(hyprctl clients -j | jq -r --argjson workspaces "$WORKSPACES" 'map(select([.workspace.id] | inside($workspaces)))')"
+  GEOM=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | slurp -o -b 11111bdd -c e64553ff -B 11111bdd -d -w 1)
+  recordfile=ScreenCapture-$(date +'%Y-%m-%d_%H:%M:%S.mp4')
+  file="$SDIR/$recordfile"
 
-if [[ -z "$GEOM" ]]; then
-  exit 0
-fi
+  if [[ -z "$GEOM" ]]; then
+    exit 0
+  fi
 
-grim -g "$GEOM" $screenshotfile
-notify-send -i $screenshotfile "󰹑 Recording started!" " $recordfile"
+  grim -g "$GEOM" $screenshotfile
+  notify-send -i $screenshotfile "󰹑 Recording started!" " $recordfile"
 
-# echo "wl-screenrec -g \"$GEOM\" -f \"$file\""
+  wl-screenrec --codec=hevc -g "$GEOM" -f "$file"
+}
 
-wl-screenrec --codec=hevc -g "$GEOM" -f "$file"
+main "$@"
