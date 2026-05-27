06e9531c7cd7970ead0c9636ee88409dd7075988
Adis Durakovic <git@mail.adisdurakovic.com>
2026-05-27 11:17:09 +0200

    M .gitignore M hyprland/.config M wm/.config ?? hyprland/.config

---
+++ hyprland/.config/hypr/config/execs.lua
@@ -0,0 +1,21 @@
+-- exec-once = uwsm app -- dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+-- exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+-- #exec-once = uwsm app -- bash -c '~/.local/scripts/wm/hyprland-desktop-portal.sh'
+-- exec-once = hyprctl setcursor Adwaita 24
+-- exec-once = uwsm app -- hyprpaper
+-- exec-once = uwsm app -- bash -c "bun ~/.config/hypr/hyprgoodies/monitors.ts"
+-- exec-once = uwsm app -- bash -c 'sleep 1 && ~/.local/scripts/wm/hyprpaper-random-wp.sh ~/Pictures/Wallpapers/Nightsky/'
+-- exec-once = uwsm app -- bash -c '/usr/libexec/polkit-gnome-authentication-agent-1'
+-- exec-once = uwsm app -- fcitx5
+-- exec-once = bash -c 'USE_LAYER_SHELL=0 vicinae server'
+-- exec-once = uwsm app -- bash -c 'eval $(gnome-keyring-daemon --start --components=ssh,secrets,pkcs11)'
+-- ; exec-once = hyprpm reload
+hl.on("hyprland.start", function()
+  -- hl.exec_cmd(terminal)
+  hl.exec_cmd("uwsm app -- dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- Execute waybar, hyprpaper, firefox
+  hl.exec_cmd("hyprctl setcursor Adwaita 24")                                            -- Execute waybar, hyprpaper, firefox
+  hl.exec_cmd("/usr/libexec/polkit-gnome-authentication-agent-1")                        -- Execute waybar, hyprpaper, firefox
+  hl.exec_cmd("fcitx5")                                                                  -- Execute waybar, hyprpaper, firefox
+  hl.exec_cmd("eval $(gnome-keyring-daemon --start --components=ssh,secrets,pkcs11)")    -- Execute waybar, hyprpaper, firefox
+end)
--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -5,3 +5,8 @@ require "config.monitors"
 require "config.input"
 require "config.keybinds"
 require "config.rules"
+
+local ok, err = pcall(require, "config.overrides")
+if not ok and not err:find("module 'config.overrides' not found") then
+  error(err)
+end
