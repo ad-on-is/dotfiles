8345a69f25787bd9e43fc0de73635cdaaa38ed3b
Adis Durakovic <git@mail.adisdurakovic.com>
2026-07-10 21:31:04 +0200

    fix animations

--- hyprland/.config/hypr/config/decorations.lua
+++ hyprland/.config/hypr/config/decorations.lua
@@ -83,8 +83,8 @@ hl.config({
 
 })
 
-hl.curve("rubber", { type = "spring", mass = 0.8, stiffness = 200, dampening = 12 })
-hl.curve("rubberSmooth", { type = "spring", mass = 0.7, stiffness = 150, dampening = 14 })
+hl.curve("rubber", { type = "spring", mass = 0.55, stiffness = 300, dampening = 16 })
+hl.curve("rubberSmooth", { type = "spring", mass = 0.55, stiffness = 150, dampening = 15 })
 
 hl.animation({ leaf = "global", enabled = true, speed = 10, spring = "rubberSmooth" })
 hl.animation({ leaf = "windows", enabled = true, speed = 10, spring = "rubber" })
